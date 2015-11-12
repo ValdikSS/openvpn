00cb9330899bf89aa60133ba2817555d0d02a3fc
ValdikSS <iam@valdikss.org.ru>
2015-11-12 17:22:04 +0300

    Remove Vista ifdefs

--- src/openvpn/win32.c
+++ src/openvpn/win32.c
@@ -1093,7 +1093,6 @@ win_get_tempdir()
   return tmpdir;
 }
 
-#if _WIN32_WINNT >= 0x0600
 bool
 win_wfp_init()
 {
@@ -1248,5 +1247,4 @@ win_wfp_block_dns (const NET_IFINDEX index)
     return true;
 }
 
-#endif
 #endif
